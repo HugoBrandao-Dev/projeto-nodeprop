cc5578d49177cf27252636e5681fe451753d894b
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-22 15:03:13 -0400

    Transformado HTML principal em arquivo EJS. Movidas as pastas css e img para dentro da pasta public, para que o express identifiqueas.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express(express)
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(8080, error => {
+  if (error) {
+    console.log('Servidor: [ Error ] => ' + error)
+  } else {
+    console.log('Servidor: [ OK ]')
+  }
+})
