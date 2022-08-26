b61c59dae1898a35f2a6fe027602edb2c2a33f38
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-26 18:12:07 -0400

    Criada rota e view para a pagina de cadastro de um novo servico.

--- controllers/ServicosController.js
+++ controllers/ServicosController.js
@@ -11,4 +11,8 @@ router.get('/admin/servicos', (req, res) => {
   res.render('admin/servicos/servicosList')
 })
 
+router.get('/admin/servico/novo', (req, res) => {
+  res.render('admin/servicos/servicoCadastrar')
+})
+
 module.exports = router
