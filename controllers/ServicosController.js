ef4a6a239c479927546b4fde6b4635daf99d583d
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-26 17:47:10 -0400

    Criada rota e view para atender a pagina de listagem de servicos, pela area administrativa.

--- controllers/ServicosController.js
+++ controllers/ServicosController.js
@@ -5,4 +5,10 @@ router.get('/servicos', (req, res) => {
   res.render('servicos')
 })
 
+/* ROTAS DO ADMINISTRADOR */
+
+router.get('/admin/servicos', (req, res) => {
+  res.render('admin/servicos/servicosList')
+})
+
 module.exports = router
