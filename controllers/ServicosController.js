bb9b0dfd0f15f7de8061b7f9d39713ce8f6867d7
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-26 21:07:21 -0400

    Criada rota para recepcao das informacoes do formulario de edicao de um servico.

--- controllers/ServicosController.js
+++ controllers/ServicosController.js
@@ -31,4 +31,18 @@ router.get('/admin/servico/edit/:id', (req, res) => {
   res.render('admin/servicos/servicoEdit')
 })
 
+router.post('/admin/servico/salvarEdicao', (req, res) => {
+  let id = req.body.iptId
+  let servico = req.body.iptServico
+  let responsaveis = req.body.iptResponsaveis
+  let informacoes = req.body.iptInformacoes
+
+  res.send({
+    id,
+    servico,
+    responsaveis,
+    informacoes,
+  })
+})
+
 module.exports = router
