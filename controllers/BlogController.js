92f1db6533c51c23d55da7731b57a3255cf7c3e3
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-28 19:44:18 -0400

    Criada rota e view para a pagina de listagem de artigos, na area administrativa.

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -5,4 +5,10 @@ router.get('/blog', (req, res) => {
   res.render('blog')
 })
 
+/* ROTAS DO ADMINISTRADOR */
+
+router.get('/admin/artigos', (req, res) => {
+  res.render('admin/artigos/artigosList')
+})
+
 module.exports = router
