58cb83d056bc953970c29a0a2f1eaa9a3540550d
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-28 12:39:17 -0400

    Criada rota para a recepcao dos dados vindos do formulario de cadastro de um novo funcionario.

--- controllers/FuncionariosController.js
+++ controllers/FuncionariosController.js
@@ -15,4 +15,32 @@ router.get('/admin/funcionarios/opcoes', (req, res) => {
   res.render('admin/funcionarios/funcionarioOpcoes')
 })
 
+router.post('/admin/funcionario/salvarNovo', (req,res) => {
+let nome = req.body.iptNome
+let nascimento = req.body.iptNascimento
+let email = req.body.iptEmail
+let setor = req.body.iptSetor
+let cargo = req.body.iptCargo
+let telefone = req.body.iptTelefone
+let celular = req.body.iptCelular
+let localizacao = req.body.iptLocalizacao
+let endereco = req.body.iptEndereco
+let informacoes = req.body.iptInformacoes
+let cpf = req.body.iptCPF
+
+  res.send({
+    nome,
+    nascimento,
+    email,
+    setor,
+    cargo,
+    telefone,
+    celular,
+    localizacao,
+    endereco,
+    informacoes,
+    cpf
+  })
+})
+
 module.exports = router
