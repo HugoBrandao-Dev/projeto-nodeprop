39d8c725a218187951350fe841e1b12d2a6190e3
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-28 14:07:41 -0400

    Criada rota para a recepcao dos dados vindos do formulario de edicao de informacoes de um funcionario cadastrado.

--- controllers/FuncionariosController.js
+++ controllers/FuncionariosController.js
@@ -47,4 +47,34 @@ router.get('/admin/funcionario/edit/:id', (req, res) => {
   res.render('admin/funcionarios/funcionarioEdit')
 })
 
+router.post('/admin/funcionario/salvarCadastrado', (req, res) => {
+  let id = req.body.iptId
+  let nome = req.body.iptNome
+  let nascimento = req.body.iptNascimento
+  let email = req.body.iptEmail
+  let setor = req.body.iptSetor
+  let cargo = req.body.iptCargo
+  let telefone = req.body.iptTelefone
+  let celular = req.body.iptCelular
+  let localizacao = req.body.iptLocalizacao
+  let endereco = req.body.iptEndereco
+  let informacoes = req.body.iptInformacoes
+  let cpf = req.body.iptCPF
+
+  res.send({
+    id,
+    nome,
+    nascimento,
+    email,
+    setor,
+    cargo,
+    telefone,
+    celular,
+    localizacao,
+    endereco,
+    informacoes,
+    cpf
+  })
+})
+
 module.exports = router
