f2bf08ba0cd697d744b909e7d40dd0cd483e2fe0
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-30 13:31:05 -0400

    Criado arquivo de conexao com o banco de dados.

---
+++ database/connection.js
@@ -0,0 +1,12 @@
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host : '127.0.0.1',
+    port : 3306,
+    user : 'root',
+    password : '',
+    database : 'nodeprop'
+  }
+});
+
+module.exports = knex
