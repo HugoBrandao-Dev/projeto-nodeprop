ab775ebfe9ad9a931479a9708acb005e32cba39e
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-30 19:48:46 -0400

    Adicionado arquivo sql de criacao da tabela de clientes_servicos, responsavel por conter as responsabilidades de cada funcionario para com a empresa.

---
+++ database/tabelas_sql/funcionarios_servicos.sql
@@ -0,0 +1,8 @@
+USE nodeprop;
+
+CREATE TABLE funcionarios_servicos (
+	funcionario_id INT UNSIGNED NOT NULL,
+	servico_id INT UNSIGNED NOT NULL,
+	CONSTRAINT fk_funcionarios FOREIGN KEY(funcionario_id) REFERENCES funcionarios(id),
+	CONSTRAINT fk_servicos FOREIGN KEY(servico_id) REFERENCES servicos(id)
+);
