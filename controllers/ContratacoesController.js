1d830ecb8074d5e3da13acbc2c571c36b0def62d
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-31 12:12:07 -0400

    Criada rota e view para a pagina de cadastro de uma nova contratacao.

--- controllers/ContratacoesController.js
+++ controllers/ContratacoesController.js
@@ -7,4 +7,8 @@ router.get('/admin/contratacoes', (req, res) => {
   res.render('admin/contratacoes/contratacoesList')
 })
 
+router.get('/admin/contratacao/nova', (req, res) => {
+  res.render('admin/contratacoes/contratacaoCadastrar')
+})
+
 module.exports = router
