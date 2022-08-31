b84c15a43167cae23c4b0f793f2cdb73b00ec4cc
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-08-31 12:17:25 -0400

    Criada rota para recepcao dos dados vindos do formulario de cadastro de uma nova contratacao.

--- controllers/ContratacoesController.js
+++ controllers/ContratacoesController.js
@@ -11,4 +11,16 @@ router.get('/admin/contratacao/nova', (req, res) => {
   res.render('admin/contratacoes/contratacaoCadastrar')
 })
 
+router.post('/admin/contratacao/salvarNova', (req, res) => {
+  let data = req.body.iptData
+  let contratante = req.body.iptContratante
+  let servico = req.body.iptServico
+
+  res.send({
+    data,
+    contratante,
+    servico
+  })
+})
+
 module.exports = router
