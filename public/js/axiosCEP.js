be2d8cee4e06241e0e908b0c1ff384a0b6e58cc1
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-01 14:39:34 -0400

    Criado script que faz a busca por endereco atraves do cep digitado no formulario de cadastro de um novo cliente.

---
+++ public/js/axiosCEP.js
@@ -0,0 +1,25 @@
+let iptLocalizacao = document.getElementsByName('iptLocalizacao')[0]
+let iptEndereco = document.getElementsByName('iptEndereco')[0]
+let iptUF = document.getElementsByName('iptUF')[0]
+
+iptCEP.onkeyup = function() {
+    if (this.value.length === 9) {
+      axios.get(`https://viacep.com.br/ws/${ aplicarMaskCEP.unmaskedValue }/json/`)
+        .then(response => {
+          iptLocalizacao.value = `${ response.data.localidade }`
+          iptEndereco.value = `Bairro: ${ response.data.bairro }, ${ response.data.logradouro }.`
+          
+          let optionsUF = [...iptUF]
+          optionsUF.forEach(option => {
+            if (option.textContent == response.data.uf) {
+              option.selected = true
+            } else {
+              option.selected = false
+            }
+          })
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
