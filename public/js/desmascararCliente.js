45300cd8b0de6352fd85788d4e40aedb0f876aec
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-01 17:30:08 -0400

    Retirado o CEP da funcao de desmascarar, para que o validator funcione corretamnte no back-end.

--- public/js/desmascararCliente.js
+++ public/js/desmascararCliente.js
@@ -2,6 +2,5 @@ function desmascarar(event, form) {
   event.preventDefault()
   iptCelular.value = aplicarMaskCelular.unmaskedValue
   iptTelefone.value = aplicarMaskTelefone.unmaskedValue
-  iptCEP.value = aplicarMaskCEP.unmaskedValue
   form.submit()
 }
