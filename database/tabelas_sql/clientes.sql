e8a45da832d1ff1811a7a93a269cdcf11dacee66
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-01 21:34:39 -0400

    Adicionado campos cep, uf, numero e nascimento para a tabela de Clientes.

--- database/tabelas_sql/clientes.sql
+++ database/tabelas_sql/clientes.sql
@@ -2,12 +2,16 @@ USE nodeprop;
 
 CREATE TABLE clientes (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nascimento DATE NOT NULL,
 	nome VARCHAR(100) NOT NULL,
 	email VARCHAR(100) NOT NULL,
 	telefone VARCHAR(100),
 	celular VARCHAR(100) NOT NULL,
+	cep VARCHAR(8) NOT NULL,
+	uf TINYINT UNSIGNED NOT NULL,
 	localizacao VARCHAR(100) NOT NULL,
 	endereco TINYTEXT NOT NULL,
+	numero INT NOT NULL,
 	informacoes_adicionais TEXT,
 	identificacao VARCHAR(100), /* CPF, CNPJ, novo RG */ 
 	PRIMARY KEY(id)
