058daf643d55f3d22813fd10ebe1d55ee231bdcd
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-01 22:11:51 -0400

    Modificado ordenamento dos campos da tabela, criado mais dois comandos SQL para consulta dos dados cadastrados e para consulta da estrutura da tabela.

--- database/tabelas_sql/clientes.sql
+++ database/tabelas_sql/clientes.sql
@@ -2,9 +2,9 @@ USE nodeprop;
 
 CREATE TABLE clientes (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	nascimento DATE NOT NULL,
 	nome VARCHAR(100) NOT NULL,
-	email VARCHAR(100) NOT NULL,
+	nascimento DATE NOT NULL,
+	email VARCHAR(100) NOT NULL UNIQUE,
 	telefone VARCHAR(100),
 	celular VARCHAR(100) NOT NULL,
 	cep VARCHAR(8) NOT NULL,
@@ -13,6 +13,9 @@ CREATE TABLE clientes (
 	endereco TINYTEXT NOT NULL,
 	numero INT NOT NULL,
 	informacoes_adicionais TEXT,
-	identificacao VARCHAR(100), /* CPF, CNPJ, novo RG */ 
+	identificacao VARCHAR(100) UNIQUE, /* CPF, CNPJ, novo RG */ 
 	PRIMARY KEY(id)
 );
+
+SELECT * FROM clientes;
+DESCRIBE clientes;
