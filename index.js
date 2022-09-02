a54286913821c5e29c6f82ea17a205adc0e39c36
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-01 21:22:02 -0400

    Importado as dependencias do express-session e express-flash e do cookieParser, para que o sistema de verificacao de formulario funcione.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 var path = require('path');
+const session = require('express-session')
+const flash = require('express-flash')
+const cookieParser = require('cookie-parser')
 
 const app = express(express)
 
@@ -7,6 +10,14 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cookieParser('qweorisadfsad'))
+app.use(session({
+  secret: "eroiwsfsdfouqwur",
+  cookie: {
+    maxAge: 60000
+  }
+}))
+app.use(flash())
 app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
 // Controllers
