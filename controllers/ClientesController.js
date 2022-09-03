9e5fb90d5346ade2fa9648cde1eadf22e6a043da
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-02 21:50:06 -0400

    Criado sistema para edicao de informacoes de um clinte ja cadastrado.

--- controllers/ClientesController.js
+++ controllers/ClientesController.js
@@ -12,75 +12,75 @@ router.get('/clientes', (req, res) => {
 
 router.get('/admin/clientes', (req, res) => {
   database.select(["id", "nome", "email", "celular"]).table('clientes')
-    .then(clientes => {
-      res.render('admin/clientes/clientesList', { clientes })
-    })
+  .then(clientes => {
+    res.render('admin/clientes/clientesList', { clientes })
+  })
 })
 
 router.get('/admin/cliente/novo', (req, res) => {
   axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
-    .then(response => {
-      let nomeError = req.flash('nomeError')
-      let anoError = req.flash('anoError')
-      let cepError = req.flash('cepError')
-      let ufError = req.flash('ufError')
-      let emailError = req.flash('emailError')
-      let telefoneError = req.flash('telefoneError')
-      let celularError = req.flash('celularError')
-      let localizacaoError = req.flash('localizacaoError')
-      let enderecoError = req.flash('enderecoError')
-      let numeroError = req.flash('numeroError')
-      let informacoesError = req.flash('informacoesError')
-      let identificacaoError = req.flash('identificacaoError')
+  .then(response => {
+    let nomeError = req.flash('nomeError')
+    let anoError = req.flash('anoError')
+    let cepError = req.flash('cepError')
+    let ufError = req.flash('ufError')
+    let emailError = req.flash('emailError')
+    let telefoneError = req.flash('telefoneError')
+    let celularError = req.flash('celularError')
+    let localizacaoError = req.flash('localizacaoError')
+    let enderecoError = req.flash('enderecoError')
+    let numeroError = req.flash('numeroError')
+    let informacoesError = req.flash('informacoesError')
+    let identificacaoError = req.flash('identificacaoError')
 
-      let nome = req.flash('nome')
-      let ano = req.flash('ano')
-      let cep = req.flash('cep')
-      let uf = req.flash('uf')
-      let email = req.flash('email')
-      let telefone = req.flash('telefone')
-      let celular = req.flash('celular')
-      let localizacao = req.flash('localizacao')
-      let endereco = req.flash('endereco')
-      let numero = req.flash('numero')
-      let informacoes = req.flash('informacoes')
-      let identificacao = req.flash('identificacao')
+    let nome = req.flash('nome')
+    let ano = req.flash('ano')
+    let cep = req.flash('cep')
+    let uf = req.flash('uf')
+    let email = req.flash('email')
+    let telefone = req.flash('telefone')
+    let celular = req.flash('celular')
+    let localizacao = req.flash('localizacao')
+    let endereco = req.flash('endereco')
+    let numero = req.flash('numero')
+    let informacoes = req.flash('informacoes')
+    let identificacao = req.flash('identificacao')
 
-      let errors = {
-        nomeError,
-        anoError,
-        cepError,
-        ufError,
-        emailError,
-        telefoneError,
-        celularError,
-        localizacaoError,
-        enderecoError,
-        numeroError,
-        informacoesError,
-        identificacaoError
-      }
+    let errors = {
+      nomeError,
+      anoError,
+      cepError,
+      ufError,
+      emailError,
+      telefoneError,
+      celularError,
+      localizacaoError,
+      enderecoError,
+      numeroError,
+      informacoesError,
+      identificacaoError
+    }
 
-      let dados = {
-        nome,
-        ano,
-        cep,
-        uf,
-        email,
-        telefone,
-        celular,
-        localizacao,
-        endereco,
-        numero,
-        informacoes,
-        identificacao
-      }
+    let dados = {
+      nome,
+      ano,
+      cep,
+      uf,
+      email,
+      telefone,
+      celular,
+      localizacao,
+      endereco,
+      numero,
+      informacoes,
+      identificacao
+    }
 
-      res.render('admin/clientes/clienteCadastrar', { response, errors, dados })
-    })
-    .catch(error => {
-      console.log(`Erro na busca pelas siglas dos estados. ${ error }.`)
-    })
+    res.render('admin/clientes/clienteCadastrar', { response, errors, dados })
+  })
+  .catch(error => {
+    console.log(`Erro na busca pelas siglas dos estados. ${ error }.`)
+  })
 })
 
 router.post('/admin/cliente/salvarNovo', (req, res) => {
@@ -133,12 +133,12 @@ router.post('/admin/cliente/salvarNovo', (req, res) => {
   let identificacaoError = null
   
   axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ uf }`)
-    .then(response => {
-      let ufOK = validator.equals(uf, response.data.id)
-      if (!ufOK) {
-        ufError = 'O estado (UF) especificado está errado.'
-      }
-    })
+  .then(response => {
+    let ufOK = validator.equals(uf, response.data.id)
+    if (!ufOK) {
+      ufError = 'O estado (UF) especificado está errado.'
+    }
+  })
 
   if (!nomeOK) {
     nomeError = 'NOME inválido ou preenchido de forma incorreta.'
@@ -217,6 +217,237 @@ router.post('/admin/cliente/salvarNovo', (req, res) => {
       informacoes_adicionais: informacoes,
       identificacao,
     }).table('clientes')
+    .then(response => {
+      res.redirect('/admin/clientes')
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+})
+
+router.get('/admin/cliente/edit/:id', (req, res) => {
+  let id = req.params.id
+  database.select().where({id}).table("clientes")
+    .then(resul => {
+      let cliente = resul[0]
+      console.log(cliente)
+    
+      // Erros
+      let idError = req.flash('idError')
+      let nomeError = req.flash('nomeError')
+      let anoError = req.flash('anoError')
+      let emailError = req.flash('emailError')
+      let telefoneError = req.flash('telefoneError')
+      let celularError = req.flash('celularError')
+      let cepError = req.flash('cepError')
+      let ufError = req.flash('ufError')
+      let localizacaoError = req.flash('localizacaoError')
+      let enderecoError = req.flash('enderecoError')
+      let numeroError = req.flash('numeroError')
+      let informacoesError = req.flash('informacoesError')
+      let identificacaoError = req.flash('identificacaoError')
+
+      let errors = {
+        idError,
+        nomeError,
+        anoError,
+        emailError,
+        telefoneError,
+        celularError,
+        cepError,
+        ufError,
+        localizacaoError,
+        enderecoError,
+        numeroError,
+        informacoesError,
+        identificacaoError
+      }
+
+      // Dados
+      let id = req.flash('id')
+      let nome = req.flash('nome')
+      let ano = req.flash('ano')
+      let email = req.flash('email')
+      let telefone = req.flash('telefone')
+      let celular = req.flash('celular')
+      let cep = req.flash('cep')
+      let uf = req.flash('uf')
+      let localizacao = req.flash('localizacao')
+      let endereco = req.flash('endereco')
+      let numero = req.flash('numero')
+      let informacoes = req.flash('informacoes')
+      let identificacao = req.flash('identificacao')
+
+      let dados = {
+        id,
+        nome,
+        ano,
+        email,
+        telefone,
+        celular,
+        cep,
+        uf,
+        localizacao,
+        endereco,
+        numero,
+        informacoes,
+        identificacao
+      }
+      axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+      .then(response => {
+        res.render('admin/clientes/clienteEdit', { cliente, response, errors, dados })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  })        
+})
+
+router.post('/admin/cliente/salvarEdicao', (req, res) => {
+  let id = req.body.iptId
+  let nome = req.body.iptNome.trim()
+  let ano = req.body.iptAno
+  let email = req.body.iptEmail
+  let telefone = req.body.iptTelefone
+  let celular = req.body.iptCelular
+  let cep = req.body.iptCEP
+  let uf = req.body.iptUF
+  let localizacao = req.body.iptLocalizacao
+  let endereco = req.body.iptEndereco
+  let numero = req.body.iptNumero
+  let informacoes = req.body.iptInformacoes
+  let identificacao = req.body.iptIdentificacao
+
+  let idOK = validator.isInt(id)
+  let nomeOK = validator.isAlpha(nome, ['pt-BR'],{
+    ignore: ' \''
+  })
+  let anoOK = validator.isDate(ano)
+  let emailOK = validator.isEmail(email)
+  let telefoneOK = validator.isMobilePhone(telefone) || validator.isEmpty(telefone)
+  let celularOK = validator.isMobilePhone(celular)
+  let cepOK = validator.isPostalCode(cep, 'BR')
+  let localizacaoOK = validator.isAlphanumeric(localizacao, ['pt-BR'], {
+    ignore: " ,.()-\':"
+  })
+  let enderecoOK = validator.isAlphanumeric(endereco, ['pt-BR'], {
+    ignore: " ,.()-\':"
+  })
+  let numeroOK = validator.isInt(numero)
+  let informacoesOK = validator.isAlphanumeric(informacoes, ['pt-BR'], {
+    ignore: " ,.()-\':"
+  }) || validator.isEmpty(informacoes)
+  let identificacaoOK = validator.isInt(identificacao)
+
+  let idError = null
+  let nomeError = null
+  let anoError = null
+  let emailError = null
+  let telefoneError = null
+  let celularError = null
+  let cepError = null
+  let ufError = null
+  let localizacaoError = null
+  let enderecoError = null
+  let numeroError = null
+  let informacoesError = null
+  let identificacaoError = null
+
+  if (!idOK) {
+    idError = 'ID inválido ou preenchido de forma incorreta.' 
+  }
+  if (!nomeOK) {
+    nomeError = 'NOME inválido ou preenchido de forma incorreta.' 
+  }
+  if (!anoOK) {
+    anoError = 'ANO DE NASCIMENTO/CRIAÇÃO inválido ou preenchido de forma incorreta.' 
+  }
+  if (!emailOK) {
+    emailError = 'EMAIL inválido ou preenchido de forma incorreta.'
+  }
+  if (!telefoneOK) {
+    telefoneError = 'TELEFONE inválido ou preenchido de forma incorreta.'
+  }
+  if (!celularOK) {
+    celularError = 'CELULAR inválido ou preenchido de forma incorreta.'
+  }
+  if (!cepOK) {
+    cepError = 'CEP inválido ou preenchido de forma incorreta.'
+  }
+  axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ uf }`)
+  .then(response => {
+    let ufOK = validator.equals(uf, response.data.id)
+    if (!ufOK) {
+      ufError = 'O estado (UF) especificado está errado.'
+    }
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  if (!localizacaoOK) {
+    localizacaoError = 'LOCALIZAÇÃO inválido ou preenchido de forma incorreta.'
+  }
+  if (!enderecoOK) {
+    enderecoError = 'ENDEREÇO inválido ou preenchido de forma incorreta.'
+  }
+  if (!numeroOK) {
+    numeroError = 'NÚMERO inválido ou preenchido de forma incorreta.'
+  }
+  if (!informacoesOK) {
+    informacoesError = 'INFORMAÇÕES inválido ou preenchido de forma incorreta.'
+  }
+  if (!identificacaoOK) {
+    identificacaoError = 'IDENTIFICAÇÃO inválido ou preenchido de forma incorreta.'
+  }
+
+  // Mensagens de erro.
+  req.flash('idError', idError)
+  req.flash('nomeError', nomeError)
+  req.flash('anoError', anoError)
+  req.flash('emailError', emailError)
+  req.flash('telefoneError', telefoneError)
+  req.flash('celularError', celularError)
+  req.flash('cepError', cepError)
+  req.flash('ufError', ufError)
+  req.flash('localizacaoError', localizacaoError)
+  req.flash('enderecoError', enderecoError)
+  req.flash('numeroError', numeroError)
+  req.flash('informacoesError', informacoesError)
+  req.flash('identificacaoError', identificacaoError)
+
+  // Variáveis a serem repassadas para o fron-end.
+  req.flash('id', id)
+  req.flash('nome', nome)
+  req.flash('ano', ano)
+  req.flash('email', email)
+  req.flash('telefone', telefone)
+  req.flash('celular', celular)
+  req.flash('cep', cep)
+  req.flash('uf', uf)
+  req.flash('localizacao', localizacao)
+  req.flash('endereco', endereco)
+  req.flash('numero', numero)
+  req.flash('informacoes', informacoes)
+  req.flash('identificacao', identificacao)
+
+  if (idError || nomeError || anoError || emailError || telefoneError || celularError || cepError || ufError || localizacaoError || enderecoError || numeroError || informacoesError || identificacaoError) {
+    res.redirect(`/admin/cliente/edit/${ id }`)
+  } else {
+    database.where({ id }).update({
+      nome,
+      nascimento: ano,
+      email,
+      telefone,
+      celular,
+      cep: cep.split('-').join(''),
+      uf,
+      localizacao,
+      endereco,
+      numero,
+      informacoes_adicionais: informacoes,
+      identificacao
+    }).table("clientes")
       .then(response => {
         res.redirect('/admin/clientes')
       })
@@ -224,37 +455,6 @@ router.post('/admin/cliente/salvarNovo', (req, res) => {
         console.log(error)
       })
   }
-})
-
-router.get('/admin/cliente/edit/:id', (req, res) => {
-  let id = req.params.id
-  res.render('admin/clientes/clienteEdit')
-})
-
-router.post('/admin/cliente/salvarEdicao', (req, res) => {
-  let id = req.body.iptId
-  let nome = req.body.iptNome
-  let ano = req.body.iptAno
-  let email = req.body.iptEmail
-  let telefone = req.body.iptTelefone
-  let celular = req.body.iptCelular
-  let localizacao = req.body.iptLocalizacao
-  let endereco = req.body.iptEndereco
-  let informacoes = req.body.iptInformacoes
-  let cpf = req.body.iptCPF
-
-  res.send({
-    id,
-    nome,
-    ano,
-    email,
-    telefone,
-    celular,
-    localizacao,
-    endereco,
-    informacoes,
-    cpf
-  })
 })
 
 router.post('/admin/cliente/deletar', (req, res) => {
