082085a09b8f951e6301e7950045bf1a848e2065
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-03 23:03:00 -0400

    Deletado sql da tabela de relacionamento cargo e setor.

--- database/tabelas_sql/setores_cargos.sql
+++
@@ -1,8 +0,0 @@
-USE nodeprop;
-
-CREATE TABLE setores_cargos (
-	setor_id INT UNSIGNED NOT NULL,
-	cargo_id INT UNSIGNED NOT NULL,
-	CONSTRAINT fk_setores FOREIGN KEY(setor_id) REFERENCES setores(id),
-	CONSTRAINT fk_cargos FOREIGN KEY(cargo_id) REFERENCES cargos(id)
-);
