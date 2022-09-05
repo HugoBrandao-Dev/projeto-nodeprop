55592be2e63dd24fd64e94e616ed6dcebf54bc5a
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-05 12:19:55 -0400

    Modificado arquivo sql da tabela de funcionarios, para que contenha os campos de cep, uf e id do setor do funcionario.

--- database/tabelas_sql/funcionarios.sql
+++ database/tabelas_sql/funcionarios.sql
@@ -5,13 +5,20 @@ CREATE TABLE funcionarios (
 	nome VARCHAR(100) NOT NULL,
 	nascimento DATE NOT NULL,
 	email VARCHAR(150) NOT NULL,
+	setor_id INT UNSIGNED NOT NULL,
 	cargo_id INT UNSIGNED NOT NULL,
 	telefone VARCHAR(13),
-	celulares VARCHAR(13) NOT NULL,
+	celular VARCHAR(13) NOT NULL,
+	cep VARCHAR(8) NOT NULL,
+	uf INT UNSIGNED NOT NULL,
 	localizacao VARCHAR(100) NOT NULL,
 	endereco TINYTEXT NOT NULL,
 	informacoes_adicionais TEXT,
 	cpf VARCHAR(100) NOT NULL,
 	PRIMARY KEY(id),
-	CONSTRAINT fk_cargos FOREIGN KEY(cargo_id) REFERENCES cargos(id)
+	CONSTRAINT fk_cargos FOREIGN KEY(cargo_id) REFERENCES cargos(id),
+	CONSTRAINT fk_setores FOREIGN KEY(setor_id) REFERENCES setores(id)
 );
+
+SELECT * FROM funcionarios;
+DESCRIBE funcionarios;
