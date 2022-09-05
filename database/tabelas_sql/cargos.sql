bdc5a1b78780cda1a21d41b5045737c47098d90a
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-05 15:18:39 -0400

    Modificado sql da tabela de cargos, fazendo com que as tabelas setores e cargos nao tenham relacao uma com a outra.

--- database/tabelas_sql/cargos.sql
+++ database/tabelas_sql/cargos.sql
@@ -3,10 +3,8 @@ USE nodeprop;
 CREATE TABLE cargos (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	cargo VARCHAR(100) NOT NULL,
-	setor_id INT UNSIGNED NOT NULL,
 	salario FLOAT NOT NULL DEFAULT 0,
-	PRIMARY KEY(id),
-	CONSTRAINT fk_setores FOREIGN KEY(setor_id) REFERENCES setores(id)
+	PRIMARY KEY(id)
 );
 
 SELECT * FROM cargos;
