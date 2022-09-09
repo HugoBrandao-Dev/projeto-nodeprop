1319f1e98a14dd62ee3078b848ffc689c0ab8ddc
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-09 14:53:10 -0400

    Modificado identificador do campo data para data_contratacao, para que nao entre em conflito com a palavra reservada.

--- database/tabelas_sql/contratacoes.sql
+++ database/tabelas_sql/contratacoes.sql
@@ -1,13 +1,13 @@
 USE nodeprop;
 
 CREATE TABLE contratacoes (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  cliente_id INT UNSIGNED NOT NULL,
-  servico_id INT UNSIGNED NOT NULL,
-  data DATE NOT NULL,
-  PRIMARY KEY(id),
-  CONSTRAINT fk_clientes FOREIGN KEY(cliente_id) REFERENCES clientes(id),
-  CONSTRAINT fk_servicos FOREIGN KEY(servico_id) REFERENCES servicos(id)
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	cliente_id INT UNSIGNED NOT NULL,
+	servico_id INT UNSIGNED NOT NULL,
+	data_contratacao DATE NOT NULL,
+	PRIMARY KEY(id),
+	CONSTRAINT fk_clientes FOREIGN KEY(cliente_id) REFERENCES clientes(id),
+	CONSTRAINT fk_servicos FOREIGN KEY(servico_id) REFERENCES servicos(id)
 );
 
 SELECT * FROM contratacoes;
