bfa26d4d9be8aecd9c2fab8db959b3cb786507cd
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-09 16:59:48 -0400

    Criado arquivo sql que contem os possiveis status que uma contratacao pode possuir.

---
+++ database/tabelas_sql/status_contratacoes.sql
@@ -0,0 +1,15 @@
+USE nodeprop;
+
+CREATE TABLE status_contratacoes (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	status_contratacao VARCHAR(50) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+SELECT * FROM status_contratacoes;
+DESCRIBE status_contratacoes;
+
+INSERT INTO status_contratacoes VALUES
+(default, "aberto"),
+(default, "am andamento"),
+(default, "finalizado");
