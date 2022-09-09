19ca45d89b6148d3e44e0caf0586621d84a3baa3
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-09 17:07:10 -0400

    Adicionado campo status_id, para armazenar os status de cada contratacao.

--- database/tabelas_sql/contratacoes.sql
+++ database/tabelas_sql/contratacoes.sql
@@ -10,5 +10,8 @@ CREATE TABLE contratacoes (
 	CONSTRAINT fk_servicos FOREIGN KEY(servico_id) REFERENCES servicos(id)
 );
 
+ALTER TABLE contratacoes ADD COLUMN status_id INT NOT NULL DEFAULT 1 AFTER servico_id;
+ALTER TABLE contratacoes ADD CONSTRAINT fk_status FOREIGN KEY(status_id) REFERENCES status_contratacoes(id);
+
 SELECT * FROM contratacoes;
 DESCRIBE contratacoes;
