60d748715bb4554e03931d2e7c0f58519bb68e61
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2022-09-30 17:22:06 -0400

    Corrigido bug de preenchimento de endereco, para que os campos de localizacao e endereco fiquem vazios, em caso do usuario digitar um cep invalido, ao inves de aparecer undefined.

--- public/js/axiosCEP.js
+++ public/js/axiosCEP.js
@@ -6,17 +6,22 @@ iptCEP.onkeyup = function() {
     if (this.value.length === 9) {
       axios.get(`https://viacep.com.br/ws/${ aplicarMaskCEP.unmaskedValue }/json/`)
         .then(response => {
-          iptLocalizacao.value = `${ response.data.localidade }`
-          iptEndereco.value = `Bairro: ${ response.data.bairro }, ${ response.data.logradouro }.`
-          
-          let optionsUF = [...iptUF]
-          optionsUF.forEach(option => {
-            if (option.textContent == response.data.uf) {
-              option.selected = true
-            } else {
-              option.selected = false
-            }
-          })
+          if (response.data.erro == 'true') {
+            iptLocalizacao.value = ''
+            iptEndereco.value = ''
+          } else {
+            iptLocalizacao.value = `${ response.data.localidade }` || ''
+            iptEndereco.value = `Bairro: ${ response.data.bairro }, ${ response.data.logradouro }.` || ''
+            
+            let optionsUF = [...iptUF]
+            optionsUF.forEach(option => {
+              if (option.textContent == response.data.uf) {
+                option.selected = true
+              } else {
+                option.selected = false
+              }
+            })
+          }
         })
         .catch(error => {
           console.log(error)
